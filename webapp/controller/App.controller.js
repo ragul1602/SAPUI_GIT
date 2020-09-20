78dffd456399db68d94644b399b339af8a1bdc90
raagul1602 <ragul1602@gmail.com>
2020-09-20 14:54:45 +0000

    Batch41_router2
    Change-Id: I0c803287acf70900828f05d5cb1b411c1479a50d

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.smax.batch41.Z_BATCH41_ROUTER.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/allTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/smax/batch41/Z_BATCH41_ROUTER/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
