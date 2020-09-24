efe8ddd6906d8842ce0b4e0df5e30cebe3020b13
ragul1602 <ragul1602@gmail.com>
2020-09-24 22:31:35 +0000

    ZPROJECT_ROUTER_DATAPASS
    Change-Id: I12d0d0ce5ce6651c24810b8104c39a965544b041

---
+++ webapp/controller/DATAPASS.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.smax.datapass.ZPROJECT_ROUTER_DATAPASS.controller.DATAPASS", {
+		onInit: function () {
+
+		},
+
+		onPress: function (oEvent) {
+
+			var oSelect = this.getView().byId("dd");
+
+			var sSelected = oSelect.getSelectedItem().getText();
+
+			// var y =  oEvent.getSource().getBindingContext().getProperty("ProductID");
+
+			// debugger;
+			this.getOwnerComponent().getRouter().navTo("PRD_DT", {
+				X : sSelected
+			});
+		}
+
+	});
+});
