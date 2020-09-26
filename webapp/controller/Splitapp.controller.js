8ba8feefcb40a9f7fc3dcf378ffe4984670ea015
ragul1602 <ragul1602@gmail.com>
2020-09-26 11:56:52 +0000

    ZPROJECT_ROUTER_SPLITAPP_POP
    Change-Id: I402cd6617bb556f389b0e3ad36ffc0bbd62a1a62

---
+++ webapp/controller/Splitapp.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("COM.SMAX.SPLITAPP.ZPROJECT_ROUTER_SPLITAPP_POP.controller.Splitapp", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/allTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"COM/SMAX/SPLITAPP/ZPROJECT_ROUTER_SPLITAPP/test/unit/controller/Splitapp.controller"
+], function () {
+	"use strict";
+});
