41d25f5450e8db07871919a0c0295b5a07b4f8c6
ragul1602 <ragul1602@gmail.com>
2020-09-26 11:52:41 +0000

    ZPROJECT_ROUTER_SPLITAPP_NFS
    Change-Id: Ia7399608996b9ee95be8cd3e76fa5ed1e456c6a0

---
+++ webapp/controller/FIRST_CUST.controller.js
@@ -0,0 +1,86 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("COM.SMAX.SPLITAPP.ZPROJECT_ROUTER_SPLITAPP_NFS.controller.FIRST_CUST", {
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf COM.SMAX.SPLITAPP.ZPROJECT_ROUTER_SPLITAPP.view.FIRST_CUST
+		 */
+		onInit: function () {
+
+			// debugger;
+			// this.getOwnerComponent().getRouter().getRoute().
+			// attachEventOnce("onItemPress",function () {
+			// 	debugger;
+			// 	var firstItem = this.getView().byId("list").getItems()[0];
+			// 	this.getView().byId("list").setSelectedItem(firstItem, true);
+
+			// }, this);
+			debugger;
+			// 			this.getView("FIRST_CUST").byId("list").attachEventOnce("onItemPress", function (oEvent) {
+
+			// 	jQuery.when(this.oUpdateFinishedDeferred).then(jQuery.proxy(function () {
+
+			// 		this.selectDetail();
+
+			// 	}, this));
+			// 	this.oUpdateFinishedDeferred.resolve();
+
+			// }, this);
+
+			// var oList = this.getView().byID('list');
+			// sap.ui.getCore().byId("__xmlview0--splApp2").setVisible(false);
+			
+			 
+			 this.getView().byId('list').attachUpdateFinished(function () {
+			 //this.getOwnerComponent().getRouter().getRoute().attachUpdateFinished(function () {
+				debugger;
+				this.getView().byId('list').getItems()[0].firePress();
+			}, this);
+
+			// this.getOwnerComponent().getRouter().getRoute("TargetSplitapp").attachPatternMatched(function (oEvent) {
+
+			// 	debugger;
+			// 	this.attachEvent('Press');
+			// 	this.fireEvent('Press');
+
+			// }, this);
+
+			// 		debugger;
+			// this.getOwnerComponent().getRouter().getRoute('TargetSplitapp').attachPatternMatched
+			// (function () {
+			// 		debugger;	
+			// 	 var firstItem = this.getView().byId("list").getItems()[0]; 
+			//       this.getView().byId("list").setSelectedItem(firstItem,true); 
+
+			// 	},this);
+
+		},
+
+		onItemPress: function (oEvent) {
+			//oEvent - press
+			debugger;
+			var custId = oEvent.getSource().getTitle();
+			this.getOwnerComponent().getRouter().navTo("Second_CustDet", {
+				custID: custId
+			});
+		},
+		
+			onPrd: function (oEvent) {
+			//oEvent - press
+			debugger;
+			// var custId = oEvent.getSource().getTitle();
+			
+			sap.ui.getCore().byId("__xmlview0--full").setVisible(false);
+			// sap.ui.getCore().byId("full").setVisible(false);
+			this.getOwnerComponent().getRouter().navTo("FIRST_PRD");
+		}
+		
+
+	});
+
+});
---
+++ webapp/controller/SECOND_PRDDET.controller.js
@@ -0,0 +1,53 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("COM.SMAX.SPLITAPP.ZPROJECT_ROUTER_SPLITAPP_NFS.controller.SECOND_PRDDET", {
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf COM.SMAX.SPLITAPP.ZPROJECT_ROUTER_SPLITAPP_NFS.view.SECOND_PRDDET
+		 */
+		onInit: function () {
+	debugger;
+				var oRouter = this.getOwnerComponent().getRouter();
+
+			oRouter.getRoute("SECOND_PRDDET").attachPatternMatched(function (oEvent) {
+			debugger;
+			var prdID = oEvent.getParameters().arguments.prdID ;
+				this.getView().bindElement("/Products("+ prdID +")");
+
+			}, this);
+		},
+
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf COM.SMAX.SPLITAPP.ZPROJECT_ROUTER_SPLITAPP_NFS.view.SECOND_PRDDET
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf COM.SMAX.SPLITAPP.ZPROJECT_ROUTER_SPLITAPP_NFS.view.SECOND_PRDDET
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf COM.SMAX.SPLITAPP.ZPROJECT_ROUTER_SPLITAPP_NFS.view.SECOND_PRDDET
+		 */
+		//	onExit: function() {
+		//
+		//	}
+
+	});
+
+});
