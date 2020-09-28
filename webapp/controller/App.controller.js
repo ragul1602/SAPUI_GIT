10beaee4a8feb10197714caaa217072e98e6f550
ragul1602 <ragul1602@gmail.com>
2020-09-28 19:39:03 +0000

    ZBATCH41_TABLES2
    Change-Id: I3a86a93b5cbf5f884badc17d05a958d9858b7cb3

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.batch41.table.ZBATCH41_TABLES.controller.App", {
+		onInit: function () {
+			
+		/*	var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData("model/employees.json");
+			this.getView().setModel(oModel);*/
+			
+			var oModel = new sap.ui.model.xml.XMLModel();
+			oModel.loadData("model/employee.xml");
+			this.getView().setModel(oModel);
+			
+			//named Model
+			
+		}
+	});
+});
